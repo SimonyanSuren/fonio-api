ab0d2b67c4a4fd3e9bbccf01be4b71afefc9e5d1
Aram Hayrapetyan <hayrapetyanaram0121@gmail.com>
2021-12-03 11:16:41 +0400

    opentact auth test

--- src/modules/opentact/opentact.auth.ts
+++ src/modules/opentact/opentact.auth.ts
@@ -57,6 +57,7 @@ export class OpentactAuth {
             }
         }
         const response = await this.axios.get("/auth", config)
+        console.log(response.data)
         if (response.data.payload.token) {
             this.jwtToken = response.data.payload.token
         }
