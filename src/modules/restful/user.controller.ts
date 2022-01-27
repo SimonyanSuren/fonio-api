2257e827480ab8367cd09a7d4fa9eddbfaf0da48
Aram Hayrapetyan <hayrapetyanaram0121@gmail.com>
2022-01-27 10:58:53 +0400

    user buy number fix

--- src/modules/restful/user.controller.ts
+++ src/modules/restful/user.controller.ts
@@ -308,7 +308,7 @@ export class UserController {
     public async buyDidNumber(@Req() req, @Res() res:  Response, @Body() body: BuyDidNumbers) {
         try {
 
-            const { paymentType, amount, orderUuid, additionalNumbers, planID, duration } = body;
+            const { paymentType, amount, orderUuid, additionalNumbers, planID, ...rest } = body;
 
             let userID = req.user?.userId,
                 companyID = req.user?.companyId,
@@ -344,7 +344,7 @@ export class UserController {
 
             let tnArray = didNumbers.payload.request?.items||numbers;
 
-            let response = await this.paymentsService.createPayment({ paymentType, amount, companyID, planID,  numberQuantity: tnArray.length, duration });
+            let response = await this.paymentsService.createPayment({ paymentType, amount, companyID, planID,  numberQuantity: tnArray.length, ...rest });
             if (response.error) {
                 return res.status(HttpStatus.BAD_REQUEST).send({ message: (response.error === '404') ? `Payment responde with status ${response.error}`: response.error });
             }
