d23aef5480eb5ad0085b4316e0c5483164f015f8
Suren <simoniansuren@gmail.com>
2022-05-19 07:29:22 +0400

    drop old invitation table

---
+++ src/modules/db/migrations/1652929606040-invitations.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class invitations1652929606040 implements MigrationInterface {
+  name = 'invitations1652929606040';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE IF EXISTS "invitations" CASCADE`);
+
+    await queryRunner.query(
+      `CREATE TYPE "public"."invitations_type_enum" AS ENUM('company_admin', 'company_user')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "invitations" ("invitation_id" SERIAL NOT NULL, "email" character varying NOT NULL, "first_name" character varying NOT NULL, "last_name" character varying NOT NULL, "company_uuid" character varying NOT NULL, "invitation_uuid" character varying NOT NULL, "type" "public"."invitations_type_enum" NOT NULL DEFAULT 'company_user', "created_on" TIMESTAMP NOT NULL DEFAULT now(), "expired_on" TIMESTAMP, "accepted_on" TIMESTAMP, CONSTRAINT "PK_56a33d475f5faf8b63d020b650f" PRIMARY KEY ("invitation_id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "invitations"`);
+    await queryRunner.query(`DROP TYPE "public"."invitations_type_enum"`);
+  }
+}
